4e37b74727ebade767712fa901a93c65ac1fb305
shubham-pyc <shubhamg2404@gmail.com>
2023-07-27 02:01:28 -0600

    Remove packages

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,26 +1,60 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { useEffect, useState } from 'react';
-import styles from './app.module.css';
-import { callHello } from '../api';
-import SignUp from './pages/singup';
-import NxWelcome from './nx-welcome';
+import { useEffect } from 'react';
+
+import 'quill/dist/quill.bubble.css';
+// import type from 'rich-text';
+
+import Quill, { DeltaOperation } from 'quill';
+import 'quill/dist/quill.bubble.css';
+import Sharedb from 'sharedb/lib/client';
+
+//@ts-expect-error
+import richText from 'rich-text';
 
 export function App() {
-  const [loading, setLoading] = useState('something');
-  // useEffect(() => {
-  //   setLoading('sdfsdf');
-  //   const fetchData = async () => {
-  //     let x = await callHello();
-  //     console.warn(x.data.message);
-  //     setLoading(x.data.message);
-  //   };
+  useEffect(() => {
+    // Register datatype to sharedb
+    Sharedb.types.register(richText.type);
 
-  //   fetchData();
-  // }, []);
+    // Make connections
+    const socket: any = new WebSocket('ws://127.0.0.1:8080');
+    const connection = new Sharedb.Connection(socket);
+
+    //Make document instance
+    const doc = connection.get('documents', 'firstDocument');
+
+    doc.subscribe(function (err) {
+      if (err) throw err;
+
+      //Quill is a document editor which gives me difference
+      let quill = new Quill('#online-editor');
+
+      quill.setContents(doc.data);
+
+      quill.on('text-change', function (delta, oldDelta, source) {
+        if (source !== 'user') return;
+        doc.submitOp(delta, { source: quill });
+      });
+
+      /** listening to changes in the document
+       * that is coming from our server
+       */
+      doc.on('op', function (op: any, source) {
+        if (source === quill) return;
+        quill.updateContents(op);
+      });
+    });
+    return () => {
+      connection.close();
+    };
+  }, []);
 
   return (
-    <SignUp></SignUp>
-    // <div>
+    <>
+      <h1>Online Text Editor</h1>
+      <div className="container">
+        <div id="online-editor"></div>
+      </div>
+    </>
   );
 }
 
